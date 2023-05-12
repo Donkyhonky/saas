e3472abfb4c045a087b6ed30ac132d4dc55ac705
John Kraft <jonnyboy80@gmx.de>
2023-05-12 14:39:16 +0200

    add comment

--- first-project/serverless-aws-react-saas/back-end/resources/markup/verification-message.ts
+++ first-project/serverless-aws-react-saas/back-end/resources/markup/verification-message.ts
@@ -1,3 +1,13 @@
+/**
+ * @constant
+ * @type {string}
+ * @description A template for a verification message to be sent to a user. This message is a complete HTML document, 
+ *              which includes a 'head' section with a defined CSS class named 'container'. This class is used in the 
+ *              'body' of the HTML to style a 'div'. Inside this div, a message is displayed to the user, which includes 
+ *              a placeholder for the verification code ({####}). This placeholder is intended to be replaced with an 
+ *              actual verification code when the message is sent.
+ */
+
 export const verificationMessage = String.raw`
     <html>
     <head>
